c26865277865b4c8e4628bf7d3145a3bb2299d56
guleraxmed <87299930+guleraxmed@users.noreply.github.com>
2021-08-17 09:53:15 +0300

    Add files via upload

---
+++ 3/script.js
@@ -0,0 +1,81 @@
+
+// function Hesabla(){
+//     let num1 = document.getElementById("num1").value;
+//     let opt = document.getElementById("opt").value;
+//     let num2 = document.getElementById("num2").value;
+
+//     document.getElementById("show").innerHTML = eval(num1 + opt + num2);
+
+// }
+function Gonder1() {
+    let num1 = document.getElementById("num1").value;
+    document.getElementById("show").innerHTML = num1;
+    
+}
+function Gonder2() {
+    let num2 = document.getElementById("num2").value;
+    document.getElementById("show").innerHTML = num2;
+    
+}
+function Gonderplus() {
+    let plus = document.getElementById("btnplus").value;
+    document.getElementById("show").innerHTML = plus;
+    
+}
+function Gonder3() {
+    let num3 = document.getElementById("num3").value;
+    document.getElementById("show").innerHTML = num3;
+    
+}
+function Gonder4() {
+    let num4 = document.getElementById("num4").value;
+    document.getElementById("show").innerHTML = num4;
+    
+}
+function Gonder5() {
+    let num5 = document.getElementById("num5").value;
+    document.getElementById("show").innerHTML = num5;
+    
+}
+function Gonder6() {
+    let num6 = document.getElementById("num6").value;
+    document.getElementById("show").innerHTML = num6;
+    
+}
+function Gondermin() {
+    let minus = document.getElementById("btnmin").value;
+    document.getElementById("show").innerHTML = minus;
+    
+}
+function Gonder7() {
+    let num7 = document.getElementById("num7").value;
+    document.getElementById("show").innerHTML = num7;
+    
+}
+function Gonder8() {
+    let num8 = document.getElementById("num8").value;
+    document.getElementById("show").innerHTML = num8;
+    
+}
+function Gonder9() {
+    let num9 = document.getElementById("num9").value;
+    document.getElementById("show").innerHTML = num9;
+    
+}
+function Gondermul() {
+    let mul = document.getElementById("btnmul").value;
+    document.getElementById("show").innerHTML = mul;
+    
+}btndiv
+function Gonder0() {
+    let num0 = document.getElementById("num0").value;
+    document.getElementById("show").innerHTML = num0;
+    
+}
+function Gonderdiv() {
+    let div = document.getElementById("btndiv").value;
+    document.getElementById("show").innerHTML = div;
+    
+}
+
+
